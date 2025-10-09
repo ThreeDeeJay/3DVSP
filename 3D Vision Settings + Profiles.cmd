7962740d57490a2a4608e1f75c8e0650687bdcdb
ThreeDeeJay <71472458+ThreeDeeJay@users.noreply.github.com>
2025-10-09 06:48:13 -0400

    Bump version to 1.3

--- 3D Vision Settings + Profiles.cmd
+++ 3D Vision Settings + Profiles.cmd
@@ -3,7 +3,7 @@ SETlocal EnableExtensions
 SETlocal EnableDelayedExpansion
 
 SET "ScriptTitle=3D Vision Settings + Profiles"
-SET "ScriptVersion=1.2"
+SET "ScriptVersion=1.3"
 SET "ScriptTitleVersion=!ScriptTitle! !ScriptVersion!"
 
 Echo [90m::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::[0m
